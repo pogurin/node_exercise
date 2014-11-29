fa5e50d8e655416a5e21ca3ceaf946e2509eb493
MitsuyoshiToguri <seiun340066@gmail.com>
2014-11-29 00:47:23 -0500

    10th

--- planetproto/100.js
+++ planetproto/100.js
@@ -1,34 +1,45 @@
 // -> Define a 'Robot' function constructor
 // -> Create two instances of Robot: 'robby' and 'cranky'
-// -> Both robby and cranky should respond to 'parts' and 'capabilities', these 
+// -> Both robby and cranky should respond to 'parts' 
+//and 'capabilities', these 
 //    should be empty arrays at first
 
-__
+function Robot() {
+	this.parts = [];
+}
+
+Robot.prototype.capabilities = [];
+
+var robby = new Robot();
+var cranky = new Robot();
 
 // -> Claim the result of robby.parts
-claim(robby.parts, __);
+claim(robby.parts, []);
 // -> Claim the result of cranky.parts
-claim(cranky.parts, __);
+claim(cranky.parts, []);
 // -> Claim the result of robby.capabilities
-claim(robby.capabilities, __);
+claim(robby.capabilities, []);
 // -> Claim the result of cranky.capabilities
-claim(cranky.capabilities, __);
+claim(cranky.capabilities, []);
 
-// -> Add 'core' to robby.parts, cranky.parts should still be empty
-// -> Add 'fly' to robby.capabilities, after doing that cranky.capabilities must 
-//    also have 'fly' without adding to it directly, so this property has to be 
+// -> Add 'core' to robby.parts, cranky.parts should 
+//still be empty
+robby.parts.push('core');
+cranky.capabilities.push('fly');
+// -> Add 'fly' to robby.capabilities, after
+// doing that cranky.capabilities must 
+//    also have 'fly' without adding to it 
+//directly, so this property has to be 
 //    shared
 
-__
-
 // -> Claim the result of robby.parts
-claim(robby.parts, __);
+claim(robby.parts,['core'] );
 // -> Claim the result of cranky.parts
-claim(cranky.parts, __);
+claim(cranky.parts, []);
 // -> Claim the result of robby.capabilities
-claim(robby.capabilities, __);
+claim(robby.capabilities, ['fly']);  
 // -> Claim the result of cranky.capabilities
-claim(cranky.capabilities, __);
+claim(cranky.capabilities, ['fly']);
 
 
 // ------------------------------------------------
